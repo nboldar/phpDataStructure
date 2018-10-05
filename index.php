fb1a361ffe7534f74c67e7193ca8cd7747e13b43
nboldar <nboldar1@gmail.com>
2018-10-06 01:17:45 +0300

    next

--- index.php
+++ index.php
@@ -50,7 +50,6 @@ if (!isset($_GET['path'])) {
 } else {
     dirInlineName($_GET['path']);
     dirNames($_GET['path']);
-
 }
 
 
---
+++ time.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nik
+ * Date: 06.10.2018
+ * Time: 0:28
+ */
+function ordinaryWay()
+{
+    $arr = [];
+    $i = 0;
+    $start = microtime(true);
+    while ($i < 2000000) {
+        array_push($arr, "a" . $i);
+        $i++;
+    }
+
+    while ($i>0){
+        array_pop($arr);
+        $i--;
+    }
+    echo 'Время выполнения скрипта: ' . (microtime(true) - $start) . ' сек.';
+}
+
+function iteratorWay()
+{
+
+    $arrIterator = new SplQueue();
+    $i = 0;
+    $start = microtime(true);
+    while ($i < 2000000) {
+        $arrIterator->enqueue("a" . $i);
+        $i++;
+    }
+    while ($i>0){
+        $arrIterator->dequeue();
+        $i--;
+    }
+    echo 'Время выполнения скрипта: ' . (microtime(true) - $start) . ' сек.';
+}
+
+ordinaryWay();
+iteratorWay();
